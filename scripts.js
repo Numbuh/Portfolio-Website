2c9df50af60f60de532ecb44561b60ca89523f7a
Numbuh <126014708+Numbuh@users.noreply.github.com>
2025-05-08 12:27:31 -0500

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const video = document.getElementById('heroVideo');
-    const volumeSlider = document.getElementById('volumeSlider');
-
-    volumeSlider.addEventListener('input', (event) => {
-        video.volume = event.target.value;
-    });
-});
